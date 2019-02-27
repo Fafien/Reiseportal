52a0c21cf849340f864cced15ce40a9c95774149
jonas <jonas@172.16.13.232>
2019-02-27 12:02:46 +0100

    Selection- & OverviewServlet bearbeitet und Serlection & Overview jsp bearbeitet

--- src/main/java/reiseportal/web/OverviewServlet.java
+++ src/main/java/reiseportal/web/OverviewServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import reiseportal.ejb.HotelBean;
 import reiseportal.jpa.Hotel;
 
@@ -22,26 +23,35 @@ import reiseportal.jpa.Hotel;
  */
 @WebServlet(name = "OverviewServlet", urlPatterns = {"/overview"})
 public class OverviewServlet extends HttpServlet {
-    public static final String URL = "/overview.html";
+    
+    public static final String URL = "/overview";
     
     @EJB
     HotelBean hotelbean;
     
-    List<Hotel> hotellist;
+    HttpSession session;
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        session = request.getSession();
+        int hotelID = (int) session.getAttribute("viewHotel");
+        
+        //TODO
+        //finde 1 Hotel über die ID (Methode in HotelBean implementieren)
+        //Hotel hotel = hotelbean.findHotelByID(ID);
+        
         request.getRequestDispatcher("/WEB-INF/overview.jsp").forward(request, response);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-        hotellist = this.hotelbean.findHotels(request.getParameter("location"));
         
-        response.sendRedirect(request.getContextPath() + OverviewServlet.URL);
+        //TODO
+        //Weiterleitung zur Buchungsseite
         
+        //response.sendRedirect(request.getContextPath() + BookingServlet.URL);
     }
 }
--- src/main/java/reiseportal/web/SelectionServlet.java
+++ src/main/java/reiseportal/web/SelectionServlet.java
@@ -6,6 +6,7 @@
 package reiseportal.web;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -13,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import reiseportal.ejb.HotelBean;
 import reiseportal.jpa.Hotel;
 
@@ -22,26 +24,42 @@ import reiseportal.jpa.Hotel;
  */
 @WebServlet(name = "Selection", urlPatterns = {"/selection"})
 public class SelectionServlet extends HttpServlet {
-    public static final String URL = "/selection.html";
+    
+    public static final String URL = "/selection";
     
     @EJB
     HotelBean hotelbean;
     
     List<Hotel> hotellist;
+    HttpSession session;
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        session = request.getSession();
+        String ort = (String) session.getAttribute("location");
+        Date from = (Date) session.getAttribute("fromDate");
+        Date until = (Date) session.getAttribute("untilDate");
+        int person = (int) session.getAttribute("persons");
+        
+        //TODO
+        //alle Hotels mit den session-Daten finden & anzeigen
+        //wenn die Liste leer ist, soll eine Fehlermeldung angezeigt werden & Weiterleitung auf index.html
+        //hotellist = new ArrayList<Hotel>();
+        hotellist = hotelbean.findHotels(ort);
+        
         request.getRequestDispatcher("/WEB-INF/selection.jsp").forward(request, response);
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-        hotellist = this.hotelbean.findHotels(request.getParameter("location"));
+        
+//        switch (request.getParameter("button"))
+//            case "hotel1":
+        session.setAttribute("viewHotel", request.getParameter("buttonOverview"));
         
         response.sendRedirect(request.getContextPath() + OverviewServlet.URL);
-        
     }
 }
