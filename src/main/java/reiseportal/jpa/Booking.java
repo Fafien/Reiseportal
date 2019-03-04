c37564310bb838ab935ef3487966536917405b92
Marwa Alqataa <maroj1991@hotmail.com>
2019-03-04 09:18:02 +0100

    Booking.java ist bearbeitet

--- src/main/java/reiseportal/jpa/Booking.java
+++ src/main/java/reiseportal/jpa/Booking.java
@@ -5,10 +5,95 @@
  */
 package reiseportal.jpa;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 /**
  *
  * @author Fabian Hupe
  */
-public class Booking {
+@Entity
+public class Booking implements Serializable {
+    @Id
+    @GeneratedValue()
+    private Long id;
+    
+    @ManyToOne
+    private Hotel hotelId = null;
+    
+    @ManyToOne
+    private Useraccount userId = null;
+    
+    private Date ankunft;
+    private Date ausreise;
+    private int personenanzahl;
+
+    public Booking(Long id, Date ankunft, Date ausreise, int personenanzahl) {
+        this.id = id;
+        this.ankunft = ankunft;
+        this.ausreise = ausreise;
+        this.personenanzahl = personenanzahl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Hotel getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Hotel hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Useraccount getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Useraccount userId) {
+        this.userId = userId;
+    }
+
+    public Date getAnkunft() {
+        return ankunft;
+    }
+
+    public void setAnkunft(Date ankunft) {
+        this.ankunft = ankunft;
+    }
+
+    public Date getAusreise() {
+        return ausreise;
+    }
+
+    public void setAusreise(Date ausreise) {
+        this.ausreise = ausreise;
+    }
+
+    public int getPersonenanzahl() {
+        return personenanzahl;
+    }
+
+    public void setPersonenanzahl(int personenanzahl) {
+        this.personenanzahl = personenanzahl;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
