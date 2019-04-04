34d7fc9384ca33dcc96d550374723a38ebd9486c
Fabian Hupe <fhupe97@googlemail.com>
2019-04-04 13:11:05 +0200

    CSS

--- src/main/java/reiseportal/web/useraccount/EmailUtil.java
+++ src/main/java/reiseportal/web/useraccount/EmailUtil.java
@@ -31,10 +31,10 @@ public class EmailUtil {
 	public static void sendEmail(String toEmail, String subject, String body){
             props = new Properties();
             props.put("mail.smtp.host", "smtp.gmail.com"); 
+            props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.socketFactory.port", "465"); 
             props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.smtp.port", "587");
+            //props.put("mail.smtp.port", "587");
                     
             auth = new Authenticator(){
                 @Override
